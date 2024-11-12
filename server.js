46da56e4332f00f901513eee59ea30f0417fe63c
Ryu Tae Hyun <ryu200112@gmail.com>
2024-11-12 22:24:07 +0900

    📤index.html 경로 변경

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const path = require("path");
 
 const app = express();
 
-// 정적 파일 제공 경로 설정
+// 정적 파일 제공 경로 설정 (public 폴더에 있는 파일들)
 app.use(express.static(path.join(__dirname, "public")));
+
+// index.html을 루트에서 제공할 수 있도록 추가 설정
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 // API 키 전달 라우트 설정
 app.get("/api-key", (req, res) => {
