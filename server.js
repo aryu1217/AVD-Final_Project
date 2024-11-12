923684bbf83220d84c8a39d6706611d1edb2994b
Ryu Tae Hyun <ryu200112@gmail.com>
2024-11-12 23:58:14 +0900

    ...

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-require("dotenv").config();
+require("dotenv").config(); // .env 파일을 로드
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
-// 정적 파일 제공 경로 설정 (docs 폴더에 있는 파일들)
-app.use(express.static(path.join(__dirname, "docs")));
+// 정적 파일 제공 경로 설정 (public 폴더 안에 있는 파일들)
+app.use(express.static(path.join(__dirname, "public")));
 
-// index.html을 루트에서 제공할 수 있도록 추가 설정
+// 루트 경로에서 index.html 파일 제공
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "docs", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// API 키 전달 라우트 설정
+app.get("/api-key", (req, res) => {
+  res.json({ apiKey: process.env.GOOGLE_MAPS_API_KEY });
 });
 
 // 서버 포트 설정 및 시작
